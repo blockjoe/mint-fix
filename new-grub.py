465debea5a8f0bdf2edc5347cec043c78f1017cc
blockjoe <69400937+blockjoe@users.noreply.github.com>
2021-02-12 16:20:19 -0500

    Update new-grub.py
    not sure if new line terminated or not, so we'll check

--- new-grub.py
+++ new-grub.py
@@ -5,9 +5,17 @@ with open('/mnt/etc/default/grub', 'r') as gf:
 new_grub = []
 for line in lines:
     if line.find("GRUB_CMDLINE_LINUX_DEFAULT") < 0:
-        new_line = line[:-1] + ' intel_idle.max_cstate=4' + line[-1]
+        if line[-1] != "\n":
+            nlt = True
+            new_line = line[:-1] + ' intel_idle.max_cstate=4' + line[-1]
+        else:
+            nlt = False
+            new_line = line[:-2] + ' intel_idle.max_cstate=4' + line[-2]
         line = new_line
     new_grub.append(line)
 
 with open('/mnt/etc/default/grub.new', 'w') as gf:
-    gf.write("\n".join(new_grub))
+    if nlt:
+        gf.write("".join(new_grub))
+    else:
+        gf.write("\n".join(new_grub))
